cefc786dac84e27681997250b932cccfdedf8053
Vishwas K Singh <vishwas@cloudthat.com>
2026-02-23 11:04:27 +0530

    Conditional Rendering

--- Fronten-apps/listingsfrontendv1/src/components/listings/ListingCard.jsx
+++ Fronten-apps/listingsfrontendv1/src/components/listings/ListingCard.jsx
@@ -3,7 +3,7 @@ function ListingCard({ listing }) {
         <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={listing.image? listing.image : "images/house.jpg"}
       alt="listing image" />
   </figure>
   <div className="card-body">
--- Fronten-apps/listingsfrontendv1/src/pages/HomePage.jsx
+++ Fronten-apps/listingsfrontendv1/src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
 import ListingCard from "../components/listings/ListingCard";
 
 let listings = [
-  { id: 1, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/ouse.jpg"},
-  { id: 2, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/ouse.jpg"},
-  { id: 3, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/ouse.jpg"},
-  { id: 4, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/ouse.jpg"}
+  { id: 1, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/house1.jpg"},
+  { id: 2, name: "vks apt", price: 25000, location:"Bengaluru", image:"public/images/house1.jpg"},
+  { id: 3, name: "vks apt", price: 25000, location:"Bengaluru"},
+  { id: 4, name: "vks apt", price: 25000, location:"Bengaluru"}
 ];
 
 export default function HomePage(){
     return(
         <div  className="grid grid-cols-3 grid-rows-none gap-4">
-          <h2>Listings</h2>
           {listings.map((listing)=>
-            <ListingCard 
+            (
+            <ListingCard  key={listing.id}
               listing={listing}
             />
+           )
           )}
         </div>
     )
