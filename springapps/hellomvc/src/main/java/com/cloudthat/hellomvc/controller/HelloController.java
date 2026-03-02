f4290f994fe1a7398ddedb1cc6d40e0e6ec022c3
Vishwas K Singh <vishwas@cloudthat.com>
2026-03-02 11:53:47 +0530

    First spring app using spring boot & Thymeleaf

--- springapps/hellomvc/src/main/java/com/cloudthat/hellomvc/controller/HelloController.java
+++ springapps/hellomvc/src/main/java/com/cloudthat/hellomvc/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.cloudthat.controller;
+package com.cloudthat.hellomvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String greet(Model model){
-        model.addAttribute("message", "Hello World Vishwas");
-        return "hello";
+        model.addAttribute("message","Hello World MVC Spring");
+        return "index";
     }
 }
